383ca5a8d77a7b7a273c3ccc1dd3f4bea5ee7f82
badreddine06 <130511207+badreddine06@users.noreply.github.com>
2025-05-06 16:53:02 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html') #commit2
   
-  @app.route("/contact/")
-   def MaPremiereAPI():
+@app.route("/contact/")
+def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
   
 if __name__ == "__main__":
